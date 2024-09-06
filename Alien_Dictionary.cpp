b1c6ac2179d1e2e2186be95df49e6abd30607710
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2024-09-06 17:16:53 +0530

    Create Alien_Dictionary.cpp

---
+++ Alien_Dictionary.cpp
@@ -0,0 +1,132 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+    
+    private:
+    vector<int> TopoSort(int V, vector<int>adj[]){
+        int indegree[V]={0};
+        for(int i=0;i<V;i++){
+            for(auto it: adj[i]){
+                indegree[it]++;
+            }
+        }
+        
+        queue<int>tracker;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                tracker.push(i);
+            }
+        }
+        
+        vector<int> topo;
+        while(!tracker.empty()){
+            int current_node = tracker.front();
+            tracker.pop();
+            topo.push_back(current_node);
+            for(auto neighbour : adj[current_node]){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0)
+                {
+                    tracker.push(neighbour);
+                }
+            }
+        }
+        
+        return topo;
+    }
+  public:
+    string findOrder(string dict[], int n, int k) {
+        // create an adjacency list
+        vector<int> adj[k];
+        
+        //traverse till n-1 and compare the pairs(i, i+1)
+        for(int i=0;i<n-1;i++){
+            //extract the pairs in string 1 and string 2 containers
+            string s1 = dict[i];
+            string s2 = dict[i+1];
+            
+            //minimum length of both the string sizes
+            int len = min(s1.size(), s2.size());
+            
+            //traverse using pointer till the minimum length
+            for(int ptr = 0; ptr<len ; ptr++){
+                //compare the characters, if unequal then create an edge
+                if(s1[ptr] != s2[ptr]){
+                    adj[s1[ptr]-'a'].push_back(s2[ptr]-'a');
+                    break;
+                }
+            }
+        }
+        
+        vector<int> topo = TopoSort(k, adj);
+        string ans="";
+        for(auto it : topo){
+            ans = ans + char(it + 'a');
+        }
+        
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+string order;
+
+bool f(string a, string b) {
+    int p1 = 0;
+    int p2 = 0;
+    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
+        p1 = order.find(a[i]);
+        p2 = order.find(b[i]);
+        //	cout<<p1<<" "<<p2<<endl;
+    }
+
+    if (p1 == p2 and a.size() != b.size())
+        return a.size() < b.size();
+
+    return p1 < p2;
+}
+
+// Driver program to test above functions
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+        string dict[N];
+        for (int i = 0; i < N; i++)
+            cin >> dict[i];
+
+        Solution obj;
+        string ans = obj.findOrder(dict, N, K);
+        order = "";
+        for (int i = 0; i < ans.size(); i++)
+            order += ans[i];
+
+        string temp[N];
+        std::copy(dict, dict + N, temp);
+        sort(temp, temp + N, f);
+
+        bool f = true;
+        for (int i = 0; i < N; i++)
+            if (dict[i] != temp[i])
+                f = false;
+
+        if (f)
+            cout << 1;
+        else
+            cout << 0;
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
